cf70009c9a8f9facaef95023d3e1f3a1051e7074
Maxim <124809701+burty7120@users.noreply.github.com>
2025-11-21 13:39:46 +0200

    Add files via upload

---
+++ src/main/java/com/example/myapplication1231231/ActivityTwo.java
@@ -0,0 +1,53 @@
+package com.example.myapplication1231231;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
+
+    String s = "відповідь прийнято";
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        btn = findViewById(R.id.btnActThree);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.btnActThree) {
+            Intent intent = new Intent(this, ActivityThree.class);
+            startActivity(intent);
+        }
+    }
+
+    public void addPointA(View v) {
+        // підрахунок поінтів
+        display(s);
+    }
+
+    public void addPointB(View v) {
+        // підрахунок поінтів
+        display(s);
+    }
+
+    public void addPointC(View v) {
+        // підрахунок поінтів
+        display(s);
+    }
+
+    public void display(String s) {
+        TextView scoreView = findViewById(R.id.txt);
+        scoreView.setText(String.valueOf(s));
+    }
+}
